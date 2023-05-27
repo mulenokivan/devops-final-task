382a9ba805d46deea3fbc84cafad43a3773b79fa
mulenokv <mulenokivan@mail.ru>
2023-05-27 15:59:55 +0700

    typofix

--- sampleproject/settings.py
+++ sampleproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 if 'SECRET_KEY' in os.environ:
     SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = ["mulenokv.dacdevops.ru/"]
+ALLOWED_HOSTS = ["mulenokv.dacdevops.ru"]
 DEBUG = True
 
 # Application definition
